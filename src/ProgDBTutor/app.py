a7fef84f91b91c43092a5a0cb26cd1cc8d98b2c9
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2022-03-09 05:26:06 +0100

    tooltips toegevoegd bij services, en service pagina (basis layout) gemaakt

--- src/ProgDBTutor/app.py
+++ src/ProgDBTutor/app.py
@@ -64,6 +64,9 @@ def login():
 def contact():
     return render_template('contact.html', app_data=app_data)
 
+@app.route("/services")
+def services():
+    return render_template('services.html', app_data=app_data)
 
 # @app.route("/show_quotes")
 # def show_quotes():
