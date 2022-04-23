f0ac16937e36be9a643caad36a14946094464150
jonasdemaeyer1 <Jonas.DeMaeyer@student.uantwerpen.be>
2022-04-23 17:50:03 +0200

    Changes in database

--- src/ProgDBTutor/user_data_acces.py
+++ src/ProgDBTutor/user_data_acces.py
@@ -25,8 +25,8 @@ class UserDataAcces:
         for row in range(amountRows):
             for column in range(amountColumns):
                 print(df.iloc[row, column])
-                cursor.execute('INSERT INTO Dataset(dataset_id, item_id, attribute, value) VALUES(%d, %d, %s, %s)',
-                (dataset_id, df.iloc[row, 0], str(df.iloc[0, column]), str(df.iloc[row, column])))
+#                 cursor.execute('INSERT INTO Dataset(dataset_id, item_id, attribute, value) VALUES(%d, %d, %s, %s)',
+#                 (dataset_id, df.iloc[row, 0], str(df.iloc[0, column]), str(df.iloc[row, column])))
 
     def get_users(self):
         cursor = self.dbconnect.get_cursor()
