5e4e5cf22ff89b97fa2f1533ee72b94611d2f4c1
jonasdemaeyer1 <Jonas.DeMaeyer@student.uantwerpen.be>
2022-04-23 17:54:27 +0200

    Changes in database

--- sql/schema.sql
+++ sql/schema.sql
@@ -50,13 +50,13 @@ CREATE TABLE Admin (
 );
 
 -- Table to keep track of the purchases of users for specific items.
-CREATE TABLE Interaction (
-    customer_id INT NOT NULL REFERENCES Customer(customer_id) ON UPDATE CASCADE ON DELETE CASCADE ,
-    item_id TEXT NOT NULL REFERENCES Item(item_id) ON UPDATE CASCADE ON DELETE CASCADE ,
-    t_dat TIME NOT NULL,
-    price INT NOT NULL,
-    PRIMARY KEY (customer_id, item_id)
-);
+-- CREATE TABLE Interaction (
+--     customer_id INT NOT NULL REFERENCES Customer(customer_id) ON UPDATE CASCADE ON DELETE CASCADE ,
+--     item_id TEXT NOT NULL REFERENCES Item(item_id) ON UPDATE CASCADE ON DELETE CASCADE ,
+--     t_dat TIME NOT NULL,
+--     price INT NOT NULL,
+--     PRIMARY KEY (customer_id, item_id)
+-- );
 
 -- Table to keep track of the datasets with their items
 CREATE TABLE Dataset (
