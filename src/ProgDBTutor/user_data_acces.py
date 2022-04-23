2cf001b1a414c0bacc793160fb2a531cef21d7c9
jonasdemaeyer1 <Jonas.DeMaeyer@student.uantwerpen.be>
2022-04-23 17:51:16 +0200

    Changes in database

--- src/ProgDBTutor/user_data_acces.py
+++ src/ProgDBTutor/user_data_acces.py
@@ -15,7 +15,7 @@ class DataScientist:
 class UserDataAcces:
     def __init__(self, dbconnect):
         self.dbconnect = dbconnect
-        cursor = self.dbconnect.get_cursor()
+#         cursor = self.dbconnect.get_cursor()
 
 #         df = pd.read_csv('/home/app/PPDB-Template-App/CSVFiles/articles.csv')
 #         amountRows = len(df.index)
