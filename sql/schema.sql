55b1ea4d842460908f490843834616039c4b5cf5
jonasdemaeyer1 <Jonas.DeMaeyer@student.uantwerpen.be>
2022-04-24 12:01:28 +0200

    Changes in database

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS CASCADE Customer, Authentication, DataScientist, Admin, Interaction , Dataset, ABTest, Users, Item;
-DROP TABLE IF EXISTS CASCADE Recommendation, Algorithm;
+DROP TABLE IF EXISTS ABTest, Recommendation, ALgorithm, Interaction, Dataset, Admin, DataScientist, Authentication, Customer
 
 CREATE TABLE Customer (
     customer_id INT PRIMARY KEY ,
