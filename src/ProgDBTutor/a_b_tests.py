05a876924b72f1a923430869357abd13133e4ec9
jonasdemaeyer1 <Jonas.DeMaeyer@student.uantwerpen.be>
2022-04-27 02:31:28 +0200

    Changes by inte

--- src/ProgDBTutor/a_b_tests.py
+++ src/ProgDBTutor/a_b_tests.py
@@ -1,10 +1,10 @@
-from app import user_data_access
+# from app import user_data_access
 
-# from config import config_data
-# from db_connection import DBConnection
-# from user_data_acces import UserDataAcces
-# connection = DBConnection(dbname=config_data['dbname'], dbuser=config_data['dbuser'])
-# user_data_access = UserDataAcces(connection)
+from config import config_data
+from db_connection import DBConnection
+from user_data_acces import UserDataAcces
+connection = DBConnection(dbname=config_data['dbname'], dbuser=config_data['dbuser'])
+user_data_access = UserDataAcces(connection)
 
 import Popularity as popularity
 
@@ -29,7 +29,43 @@ def startAB(abtest_id, dataset_id=None):
             popAlgo.recommend()
 
 
-def main():
-    startAB(1, 0)
+def getABtestResults(abtest_id, dataset_id):
 
-# main()
+    abtest = user_data_access.getAB_Test(abtest_id)
+    topk = abtest.topk
+    start_point = abtest.start_point
+    date_tuple = getDate(start_point)
+
+    #TODO: uit algorithm: name en param_name (zoeken op abtest_id en result_id)
+    # uit recommendation: item_ids en end_point (zoeken op abtest_id, result_id en dataset_id)
+
+    cursor = user_data_access.dbconnect.get_cursor()
+    cursor.execute('SELECT DISTINCT result_id FROM Result WHERE dataset_id = %s', (str(dataset_id)))
+    results = [['TOP-K', 'Current Date', 'Result_id', 'Algorithm name', 'Window']]
+    for i in range(topk):
+        results[0].append('Item' + str(i+1))
+
+    algorithm = []
+
+    resultIdList = list()
+    for row in cursor:
+        resultIdList.append(row[0])
+
+    print(resultIdList)
+
+    itemEndPoints = list()
+    for res in resultIdList:
+        cursor.execute("SELECT item_id, end_point FROM Recommendation WHERE abtest_id = %s AND dataset_id = %s AND result_id= %s", (abtest_id, dataset_id, res))
+        for row in cursor:
+            itemEndPoints.append([row[0], getDate(row[1])])
+
+    print(itemEndPoints)
+    return results
+
+def getDate(date):
+    return int(date.year), int(date.day), int(date.day)
+
+def main():
+    getABtestResults(1, 0)
+
+main()
