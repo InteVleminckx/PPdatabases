db540fb8f75cd45c3b92c7b06e3cfb7f4e80a098
InteVleminckx <inte.vleminckx14@hotmail.com>
2022-05-20 15:26:37 +0200

    Toevoegen van de purchases en active users per dag en totaal over de AB test periode

--- src/ProgDBTutor/visualisation.py
+++ src/ProgDBTutor/visualisation.py
@@ -10,7 +10,7 @@ from Metrics import *
 
 def getInfoVisualisationPage(abtest_id, dataset_id):
     cursor = dbconnect.get_cursor()
-    cursor.execute("select start_point, end_point, stepsize, topk from abtest where abtest_id = %s limit 1;", (str(abtest_id)))
+    cursor.execute("select start_point, end_point, stepsize, topk from abtest where abtest_id = %s limit 1;", (str(abtest_id),))
     row = cursor.fetchone()
 
     if row is None:
@@ -22,10 +22,10 @@ def getInfoVisualisationPage(abtest_id, dataset_id):
     topk = str(row[3])
 
     datasetName = getDatasetname(dataset_id)
-    graphPurchasesAndUsers = getPurchasesAndActiveUsersOverTime(startPoint, endPoint)
+    graphPurchasesAndUsers,totalUsers, totalPurch = getPurchasesAndActiveUsersOverTime(startPoint, endPoint)
 
     return {"abtest_id": abtest_id, "startpoint":startPoint, "endpoint":endPoint, "datasetname": datasetName,
-            "stepsize": stepsize, "topk": topk, "graphPurchAndUsers" : graphPurchasesAndUsers}
+            "stepsize": stepsize, "topk": topk, "graphPurchAndUsers" : graphPurchasesAndUsers, "totalUsers": totalUsers, "totalPurchases": totalPurch}
 
 def getPurchasesAndActiveUsersOverTime(start, end):
 
@@ -37,7 +37,11 @@ def getPurchasesAndActiveUsersOverTime(start, end):
         return None
 
     info = {}
+    totalusers = 0
+    totalPurch = 0
     for row in rows:
         info[str(row[2])[0:10]] = {"purchases": str(row[1]), "users": str(row[0])}
+        totalusers += int(row[0])
+        totalPurch += int(row[1])
 
-    return info
+    return info, str(totalusers), str(totalPurch)
