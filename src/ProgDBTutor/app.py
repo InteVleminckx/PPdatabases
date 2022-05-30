fd88bc9143c407aad6b2651eabae2ae6a301e801
InteVleminckx <inte.vleminckx14@hotmail.com>
2022-05-30 22:04:45 +0200

    estimated time integrated

--- src/ProgDBTutor/app.py
+++ src/ProgDBTutor/app.py
@@ -210,6 +210,7 @@ def services():
                                                     algo_list[j][3])
 
                         del algo_dict[str(i)]
+                        algo_times['times'][current_id] = 0
                         current_id += 1
 
             # Remove algorithms from list and dicts
